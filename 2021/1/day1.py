e17c8846301b47cf2b53748be9ba703c8d77dba8
flomero <florianml.fischer@fau.de>
2021-12-03 12:16:05 +0100

    🎅 readme etc

--- 2021/1/day1.py
+++ 2021/1/day1.py
@@ -1,16 +1,15 @@
-file = "C:/Users/flofi/repos/CodeImAdvent/2021/1/" + "data.txt"
+file = "C:/Users/flofi/repos/CodeImAdvent/2021/1/" + "test.txt"
 
-with open(file, 'r') as f:
-  list = f.read().splitlines()
+with open(file, "r") as f:
+    data = f.read()
+    data = data.split("\n")
 
-print(list)  
+count = 0
 
-counter = 0
+for i in range(len(data)-3):
+    prev = int(data[i]) + int(data[i+1]) + int(data[i+2])
+    nextOne = int(data[i+1]) + int(data[i+2]) + int(data[i+3])
+    if nextOne>prev:
+        count = count + 1
 
-for i in range(len(list)):
-    if i < len(list) - 1:
-        if list[i] < list[i+1]:
-            counter = counter + 1
-
-print(counter)
-
+print(count)
