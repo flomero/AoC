ba9ae89a26f9a6856f17bbdac49145c2d3520ebe
flomero <florianml.fischer@fau.de>
2021-12-05 00:37:08 +0100

    🎅 day 4 - 2

--- 2021/4/day4-2.py
+++ 2021/4/day4-2.py
@@ -1,39 +1,41 @@
+from typing import Counter
 import numpy as np
 import pandas as pd
 
-source = "test"
+source = "data"
 file = "C:/Users/flofi/repos/CodeImAdvent/2021/4/" + source + ".txt"
 
 input = np.loadtxt(file, delimiter=",", max_rows=1).astype(int)
 boards = np.loadtxt(file, skiprows=1).reshape((-1,5,5)).astype(int)
 
-winboards = []
-winnumber = ()
-prev = []
+number = ()
 losboard = []
+values = [False] * len(boards)
 stop = False
 
 for i in input:
   for j, b in enumerate(boards):
-    b[b == i] = -i
-    filtered = (b < 0)
-    if filtered.all(axis = 0).any() or filtered.all(axis = 1).any():
-      winboards.append(b)
-      if winboards == prev:
-          losboard = b
-          winnumber = i
-          stop = True
-      else: 
-        prev = winboards
+    if values[j] == False:
+        if i != 0:
+            b[b == i] = -i
+        else:
+            b[b == i] = -1
+        filtered = (b < 0)
+        if filtered.all(axis = 0).any() or filtered.all(axis = 1).any():
+            values[j] = True
+        if values.count(False) == 0:
+            losboard = b
+            number = i
+            stop = True
+            break
     if stop:
       break
   if stop:
     break
 
 print(losboard)
-print(winnumber)
-
-result = winnumber * np.sum(losboard[losboard > 0])
-
+print(number)
+sum = np.sum(losboard[losboard > 0])
+print(sum)
+result = number * sum
 print(result)
-print(winboards)
