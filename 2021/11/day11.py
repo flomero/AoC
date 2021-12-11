361fab30cfa79aedc405a8743420a9d9004edc1e
flomero <florianml.fischer@fau.de>
2021-12-11 14:49:48 +0100

    🎅 day 11 animation

--- 2021/11/day11.py
+++ 2021/11/day11.py
@@ -1,10 +1,16 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 source = "data"
 file = "C:/Users/flofi/repos/CodeImAdvent/2021/11/" + source + ".txt"
 input = np.genfromtxt(file, delimiter = 1, dtype=int)
 
+fig = plt.gcf()
+im = plt.imshow(input)
+
 steps = 1000
+f = steps
 
 def addns(arr, i, j):
     if i == 0 or i == len(arr) - 1 or j == 0 or j == len(arr[i]) - 1:
@@ -36,6 +42,7 @@ def addns(arr, i, j):
     return
 
 blinks = 0
+all = [input]
 
 for s in range(steps):
     input = np.add(input, 1)
@@ -53,9 +60,21 @@ for s in range(steps):
                        for b in blinked:
                             input[b[0]][b[1]] = 0 
     blinks = blinks + len(blinked)
+    all.append([input])
     if len(blinked) == 100:
         print(s + 1)
+        f = s
+        all.append([input])
         break
 
+def animate(frame):
+    this = all[frame]
+    print(this)
+    this =np.squeeze(this)
+    im = plt.imshow(this)
+    return im
+
+anim = FuncAnimation(fig, animate, frames = f, interval = 50)
+anim.save("C:/Users/flofi/repos/CodeImAdvent/2021/11/animation.gif")
 print(input)
 print(blinks)
