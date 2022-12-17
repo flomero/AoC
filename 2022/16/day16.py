c380f2ff56cbe08aa1c452e1118d13426c004709
flomero <florianml.fischer@fau.de>
2022-12-17 21:14:25 +0100

    🚧 day 16&17 wip

--- 2022/16/day16.py
+++ 2022/16/day16.py
@@ -4,7 +4,7 @@ from itertools import combinations, permutations
 import matplotlib.pyplot as plt
 
 day = "16"
-source = "test"
+source = "data"
 file = "C:/Users/flofi/repos/CodeImAdvent/2022/" + day + "/" + source + ".txt"
 
 valves = {}
@@ -34,27 +34,100 @@ for v in valves:
 
 nx.draw(G, with_labels=True)
 
+print("graph donw")
+
 nonZeroValves = []
 for v in valves:
     if valves[v]["rate"] > 0:
         nonZeroValves.append(v)
 
-pairs = list(list(l) for l in combinations(nonZeroValves, 2))
-for p in range(len(pairs)):
-    l = len(nx.shortest_path(G, source=pairs[p][0], target=pairs[p][1])) - 1
-    pairs[p].append(l)
+print("nzv done")
 
-start = "AA"
-ways = list(list(i) for i in permutations(nonZeroValves))
-for w in ways:
-    print(w)
+paths = [list(i) for i in combinations(nonZeroValves, 2)]
+connections = {}
+for p in paths:
+    left = p[0]
+    right = p[1]
+    length = len(nx.shortest_path(G, source=left, target=right))
+    connections[str(left + right)] = length
+    connections[str(right + left)] = length
 
-def checkWay(w):
-    mins = len(nx.shortest_path(G, source="AA", target=w[0])) - 1
-    while mins < 30:
-        for p in range(len(w) - 1):
-            
+ways = []
+
+## def checkSeq(seq: list, count: int):
+##     global ways
+##     for p in nonZeroValves:
+##         if len(seq) == len(nonZeroValves):
+##             ways.append(seq)
+##             break 
+##         if p not in seq:
+##             count += connections[str(seq[-1] + p)]
+##             if count > 30:
+##                 ways.append(seq)
+##                 break 
+##             seq.append(p)
+##             checkSeq(seq, count)
+
+## for p in nonZeroValves:
+##    checkSeq([p], 0)
 
 
 
+## print(len(ways))
+## 
+## print("ways done")
+
+def checkWay(w: list):
+    mins = len(nx.shortest_path(G, source="AA", target=w[0]))
+    openValves = list()
+    flow = 0
+    for p in range(len(w)):
+        mins += 1
+        openValves.append(int(valves[w[p]]["rate"]))
+        flow = flow + sum(openValves)
+        ## print(mins, flow, sum(openValves))
+        if mins == 30: break
+        if (p+1) <= len(w) - 1:
+            way = len(nx.shortest_path(G, source=w[p], target=w[p+1])) - 1
+            for _ in range(way):
+                mins += 1
+                flow = flow + sum(openValves)
+                if mins == 30: break
+                ## print(mins, flow, sum(openValves))
+        if p == len(w)-1:
+            while True:
+                mins += 1
+                flow = flow + sum(openValves)
+                ## print(mins, flow, sum(openValves))
+                if mins == 30: break 
+        if mins == 30: break 
+        ## print(sum(openValves))
+    return flow
+partOne = 0
+## for w in ways:
+##     score = checkWay(w)
+##     if score > partOne:
+##         partOne = score
+## 
+## print(partOne)
+## 
+## 
 ## plt.show()
+iterator = permutations(nonZeroValves)
+
+test = 0
+res = 0
+
+for it in iterator:
+    count = 0
+    for i in range(1, len(it)):
+        count += connections[str(it[i] + it[i-1])]
+    if count <= 30:
+        way = checkWay(list(it))
+        test += 1
+        print(test)
+        if way > res:
+            res = way
+
+
+print(res)
