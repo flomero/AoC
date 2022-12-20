9c2b4b287549278348deef74551c2895efbcc6a3
flomero <florianml.fischer@fau.de>
2022-12-20 21:28:32 +0100

    🚧 day20 wip

--- 2022/18/day18.py
+++ 2022/18/day18.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 day = "18"
-source = "test"
+source = "data"
 file = "C:/Users/flofi/repos/CodeImAdvent/2022/" + day + "/" + source + ".txt"
 
 input = np.genfromtxt(file, dtype=int, delimiter=",")
@@ -17,20 +18,56 @@ for i in input:
         if not np.any(np.all(input == b, axis=1)):
             visible += 1
 
-print(visible)
+partOne = visible
+print(partOne)
 
 max = np.amax(input, axis=0)
-
 min = np.amin(input, axis=0)
 
 x = max[0] + 1
 y = max[1] + 1
 z = max[2] + 1
-
-print(x, y, z)
-
 map = np.zeros(shape=(x, y, z))
-print(map)
 
 for i in input:
-    map[np.array(i)] = 1
+    x = i[0]
+    y = i[1]
+    z = i[2]
+    map[x][y][z] = 1
+
+start = np.array([0,0,0])
+
+def checkNext(point):
+    x = point[0]
+    y = point[1]
+    z = point[2]
+    if map[x][y][z] == 1:
+        return
+    elif map[x][y][z] == 0:
+        map[x][y][z] = 2
+        for d in dirs:
+            try:
+                new = point + d
+                checkNext(new)
+            except:
+                pass
+        
+
+np.set_printoptions(threshold=sys.maxsize)
+checkNext(start)
+
+faces = 0
+
+air = zip(*np.where(map == 0))
+for a in (list(a) for a in air):
+    x = a[0]
+    y = a[1]
+    z = a[2]
+    point = np.array([x,y,z])
+    for d in dirs:
+        b = point + d
+        if np.any(np.all(input == b, axis=1)):
+            faces += 1
+
+partTwo = partOne - faces
+print(partTwo)
--- 2022/20/day20.py
+++ 2022/20/day20.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-day = "17"
+day = "20"
 source = "test"
 file = "C:/Users/flofi/repos/CodeImAdvent/2022/" + day + "/" + source + ".txt"
 
-with open(file) as file:
-    input = file.readline()
+input = np.genfromtxt(file, delimiter="\n", dtype=int)
+print(input)
