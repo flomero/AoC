ade806e765fa011ecbc7d6efc665b104979e48a1
flomero <florianml.fischer@fau.de>
2023-12-15 15:54:32 +0100

    day 15 part 2

--- 2023/15/day15.py
+++ 2023/15/day15.py
@@ -1,5 +1,8 @@
+import pprint
+
+
 day = "15"
-source = "test"
+source = "data"
 file = "C:/Users/flofi/repos/CodeImAdvent/2023/" + day + "/" + source + ".txt"
 
 input = ""
@@ -20,10 +23,38 @@ def hash(str):
 
 for i in input: 
     value = hash(i)
-    print(value)
     result += value
 
 print(result)
 
 # Part 2
+boxes = {i:[] for i in range(256)}
 
+for i in input: 
+    if "=" in i: 
+        l,f = i.split("=")
+        lens = (l, int(f))
+        labels = [x[0] for x in boxes[hash(l)]]
+        if l not in labels:
+            boxes[hash(l)].append(lens)
+        else:
+            for x in boxes[hash(l)]: 
+                if x[0] == l: 
+                    boxes[hash(l)][boxes[hash(l)].index(x)] = lens
+    elif "-" in i: 
+        l = i.split("-")[0]
+        box = hash(l)
+        labels = [x[0] for x in boxes[box]]
+        if l in labels: 
+            for x in boxes[box]: 
+                if x[0] == l: 
+                    boxes[box].remove(x)
+
+focusPower = 0
+for box in boxes: 
+    for index, lens in enumerate(boxes[box]): 
+        power = (index + 1) * (box + 1) * lens[1]
+        focusPower += power
+
+print(focusPower)
+        
