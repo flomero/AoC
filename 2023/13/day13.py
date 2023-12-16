0aece243fcfbfc5e187ceb495b294b83d096fbdb
flomero <florianml.fischer@fau.de>
2023-12-16 09:19:10 +0100

    day 13 wip

--- 2023/13/day13.py
+++ 2023/13/day13.py
@@ -10,37 +10,51 @@ with open(file) as f:
 input = input.split("\n\n")
 input = [i.split("\n") for i in input]
 
-pprint.pprint(input)
+result = 0
 
-def getReflection(map):
+#pprint.pprint(input)
+
+def transpose(arr):
+    return [[arr[j][i] for j in range(len(arr))] for i in range(len(arr[0]))]
+
+def findReflection(arr):
     matches = []
-    for i in range(len(map)):
-        for j in range(len(map)):
-            if i == j:
-                continue
-            if map[i] == map[j]:
-                matches.append(i)
-                matches.append(j)
-    return matches
+    # find matching rows
+    for i in range(len(arr)): 
+        for j in range(len(arr)): 
+            if i < j: 
+                if arr[i] == arr[j]: 
+                    matches.append((i+1,j+1))
+    if len(matches) == 0:
+        return 0
+    if (len(arr)) not in [i[1] for i in matches] and 1 not in [i[0] for i in matches]:
+        return 0
+    if len(matches) == 1:
+        return matches[0][0] + 1
+    # find start
+    start = (0,0)
+    print(matches)
+    for i in matches:
+        if i[0] == 1 or i[1] == len(arr):
+            start = i
+    #print(start)
+    #print(matches)
+    # check continuity
+    g = int((start[1] - start[0] -1) / 2)
 
-def transpose(map):
-    # change rows to columns
-    return ["".join([map[i][j] for i in range(len(map))]) for j in range(len(map))]
-    
+    if g == 0:
+        return 0
+    #print(g)
+    for x in range(1,g+1):
+        print((start[0] + x, start[1] - x))
+        if (start[0] + x, start[1] - x) not in matches:
+            return 0
+    return start[0] + g
 
-for map in input:
-    pprint.pprint(map)
-    pprint.pprint(transpose(map))
-    vertical = getReflection(map)
-    horizontal = getReflection(transpose(map))
-    print(horizontal)
-    print(vertical)
-    if len(map) in horizontal and len(map) - 1 in horizontal:
-        print("horizontal")
-    elif 0 in vertical and 1 in vertical:
-        print("horizontal")
+for i in input:
+    x = findReflection(i) * 100 + findReflection(transpose(i))
+    print(x)
+    result += x
     
-    if len(transpose(map)) in vertical and len(transpose(map)) - 1 in vertical:
-        print("vertical")
-    elif 0 in horizontal and 1 in horizontal:
-        print("vertical")
+print("Result: ")
+print(result)
