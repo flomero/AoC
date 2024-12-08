37c3c3e5fef6925fc7d1a027466cfbc91a8df114
flomero <49640251+flomero@users.noreply.github.com>
2024-12-08 07:17:30 +0100

    part2

--- 2024/08/solution.py
+++ 2024/08/solution.py
@@ -4,6 +4,8 @@ day = "08"
 source = "test"
 file = f"C:/Users/flofi/repos/CodeImAdvent/2024/{day}/{source}.txt"
 
+part = 1
+
 with open(file, "r") as f:
 	input_text = f.read()
 
@@ -40,21 +42,47 @@ def add_antinodes_for_frequency(positions, frequency):
 			if 0 <= antinode2[0] < height and 0 <= antinode2[1] < width:
 				antinodes.add(antinode2)
 
+def add_antinodes_for_frequency_parttwo(positions, frequency):	
+	n = len(positions)
+	for i in range(n):
+		for j in range(i + 1, n):
+			y1, x1 = positions[i]
+			y2, x2 = positions[j]
+			
+			dy = y2 - y1
+			dx = x2 - x1
+
+			while 0 <= y1 < height and 0 <= x1 < width:
+				antinode = (y1, x1, frequency)
+				antinodes.add(antinode)
+				y1 += dy
+				x1 += dx
+			while 0 <= y2 < height and 0 <= x2 < width:
+				antinode = (y2, x2, frequency)
+				antinodes.add(antinode)
+				y2 -= dy
+				x2 -= dx
+
 for freq, positions in antennas.items():
-	add_antinodes_for_frequency(positions, freq)
-	for pos in positions:  
-		antinodes.add((pos[0], pos[1], freq))
+	if part == 1:
+		add_antinodes_for_frequency(positions, freq)
+	elif part == 2:
+		add_antinodes_for_frequency_parttwo(positions, freq)
+	if len(positions) > 1:
+		for pos in positions:
+			antinodes.add((pos[0], pos[1], freq))
 
 output_lines = list(lines)
 
 counter = 0
 for y, x, origin_freq in antinodes:
-	if output_lines[y][x] == ".": 
+	if output_lines[y][x] == ".":
 		output_lines[y] = output_lines[y][:x] + "#" + output_lines[y][x+1:]
 		counter += 1
-	elif output_lines[y][x].isalnum() and output_lines[y][x] != origin_freq: 
-		output_lines[y] = output_lines[y][:x] + "#" + output_lines[y][x+1:]
-		counter += 1
+	elif output_lines[y][x].isalnum():
+			if part == 2 or output_lines[y][x] != origin_freq:
+				output_lines[y] = output_lines[y][:x] + "#" + output_lines[y][x+1:]
+				counter += 1
 
 pprint(output_lines)
 print("Total antinodes:", counter)
